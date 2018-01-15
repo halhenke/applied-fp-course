7898c2294f9303b89e82dd95a9714cbf6546f9b4
Sean Chalmers <sean.chalmers@data61.csiro.au>
2018-01-15 21:20:55 +1000

    Removed import of Conf from Tests on level 03

--- level03/tests/Test.hs
+++ level03/tests/Test.hs
@@ -10,50 +10,39 @@ import qualified System.Exit                as Exit
 
 import qualified Data.ByteString.Lazy.Char8 as LBS8
 
-import qualified FirstApp.Conf              as Conf
 import qualified FirstApp.Main              as Main
 
 main :: IO ()
 main = do
-  -- We're still going to be running our application so we need to load the config.
-  cfgE <- Conf.parseOptions "appconfig.json"
-  case cfgE of
+  -- We need to setup our Application.
+  let app' = pure Main.app
 
-    Left err ->
-      -- Die loudly if the config couldn't be loaded. Ensure we exit with a
-      -- failure code to trip up anything that might be watching our run status.
-      error "Config failure error handling not implemented"
+  -- This sets up HSpec to use our application as the thing it executes before the tests are run
+  hspec . with app' $ do
+      -- Here is an example test for the 'ListRq' route.
+      -- Start with a general description of what we're going to test.
+      describe "List Route" $ do
+        -- Individual test cases provide more precise information regarding
+        -- what they are going to test.
+        it "Should return a 'not implemented' message and 200 status" $
+          -- Using the functions from ``Test.Hspec.Wai`` this actions a GET request
+          -- on the "/list" route, and using an infix function, compares the result of
+          -- that request to our expected result.
 
-    Right cfg -> do
-      -- We need to setup our Application.
-      let app' = pure (Main.app cfg)
+          -- There String literal here is being converted by the use of the
+          -- ``IsString`` typeclass into a response type that Hspec.Wai can
+          -- use. Check the documentation for more examples, but when given
+          -- a string literal, it will assume that is the expected body of
+          -- the request and also check for a 200 response code.
+          get "/list" `shouldRespondWith` "List Request not implemented"
 
-      -- This sets up HSpec to use our application as the thing it executes before the tests are run
-      hspec . with app' $ do
-          -- Here is an example test for the 'ListRq' route.
-          -- Start with a general description of what we're going to test.
-          describe "List Route" $ do
-            -- Individual test cases provide more precise information regarding
-            -- what they are going to test.
-            it "Should return a 'not implemented' message and 200 status" $
-              -- Using the functions from ``Test.Hspec.Wai`` this actions a GET request
-              -- on the "/list" route, and using an infix function, compares the result of
-              -- that request to our expected result.
+      -- Write some more tests, below are some ideas to get you started:
 
-              -- There String literal here is being converted by the use of the
-              -- ``IsString`` typeclass into a response type that Hspec.Wai can
-              -- use. Check the documentation for more examples, but when given
-              -- a string literal, it will assume that is the expected body of
-              -- the request and also check for a 200 response code.
-              get "/list" `shouldRespondWith` "List Request not implemented"
+      -- Don't worry if you don't get all of these done. :)
 
-          -- Write some more tests, below are some ideas to get you started:
-
-          -- Don't worry if you don't get all of these done. :)
-
-          -- 1) The '<topic>/add' route will respond with an error when given an empty comment
-          -- 2) The '<topic>/view' route will respond correctly when given a topic
-          -- 3) The '<topic>/view' route will respond with an error when given an empty topic
-          -- 4) A gibberish route will return a 404
-          -- 5) The '<topic>/add' route will respond with the message from the config (the `mkMessage` function from FirstApp.Conf will help)
+      -- 1) The '<topic>/add' route will respond with an error when given an empty comment
+      -- 2) The '<topic>/view' route will respond correctly when given a topic
+      -- 3) The '<topic>/view' route will respond with an error when given an empty topic
+      -- 4) A gibberish route will return a 404
+      -- 5) The '<topic>/add' route will respond with the message from the config (the `mkMessage` function from FirstApp.Conf will help)
 
